f31485c0c25553588216f1943787da6d78b757b5
tien22012003 <tien.2174802010338@vanlanguni.vn>
2026-06-16 00:25:43 +0700

    Fix uploads static folder

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-
+app.use("/uploads", express.static("uploads"));
 app.use(
   session({
     secret: process.env.SESSION_SECRET || "food_order_secret",
